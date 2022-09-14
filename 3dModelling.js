85bff81fdbd426eab67238e30514d8a81734cbb2
Revtrov <ryanmgold2017@gmail.com>
2022-09-14 20:48:45 +0100

    initial

---
+++ 3dModelling.js
@@ -0,0 +1,115 @@
+import * as THREE from './three.module.js';
+import { OrbitControls } from './OrbitControls.js';
+import { dataBase } from './readDataBase.js';
+
+let totalOfRadii = 0;
+for (let i = 0; i < dataBase.radii.length; i++) {
+    if (dataBase.radii[i] == "" && dataBase.radii[i] != undefined) {
+        continue
+    } else {
+        totalOfRadii += parseFloat(dataBase.radii[i])
+    }
+}
+let averagePlanetRadius = totalOfRadii / dataBase.radii.length
+
+const scene = new THREE.Scene(),
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.0001, 70000),
+    renderer = new THREE.WebGLRenderer({
+        canvas: document.getElementById("canvas"),
+    }),
+    controls = new OrbitControls(camera, renderer.domElement);
+controls.update()
+let pixality = 1;
+let brightness = 0.7;
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+camera.position.set(0, 2, 20);
+
+const ambientLight = { light: new THREE.AmbientLight(0xfffff3, brightness) },
+    cubeLight = new THREE.PointLight(0x00ff00, 0.1, 1, 1),
+    pointLight = new THREE.PointLight(0xfcba03, 0.1, 1, 1),
+    lightHelper = new THREE.PointLightHelper(cubeLight),
+    gridHelper = new THREE.GridHelper(500, 500);
+
+pointLight.position.set(0, 0, 0)
+scene.add(pointLight, ambientLight.light, /*lightHelper gridHelper*/ );
+
+let skyboxRadius = 900
+const skyboxTexture = new THREE.TextureLoader().load('textures/skybox.jpg');
+const skybox = new THREE.SphereGeometry(skyboxRadius, 320, 160);
+const skyboxMaterial = new THREE.MeshStandardMaterial({ map: skyboxTexture, side: THREE.DoubleSide });
+const skyboxMesh = new THREE.Mesh(skybox, skyboxMaterial);
+scene.add(skyboxMesh)
+skyboxMesh.position.set(0, 0, 0)
+
+let spacing = 1;
+let earthRadius = 1
+const earthTexture = new THREE.TextureLoader().load('textures/earth.jpg');
+const earth = new THREE.SphereGeometry(earthRadius, 32, 16);
+const earthMaterial = new THREE.MeshStandardMaterial({ map: earthTexture });
+const EarthMesh = new THREE.Mesh(earth, earthMaterial);
+scene.add(EarthMesh)
+EarthMesh.rotateZ((Math.PI / 180) * 16);
+EarthMesh.position.set(+((averagePlanetRadius + earthRadius) / 2) + spacing, 0, 0)
+
+const averagePlanetTexture = new THREE.TextureLoader().load('textures/averagePlanet.jpg');
+const averagePlanet = new THREE.SphereGeometry(averagePlanetRadius, 320, 160);
+const averagePlanetMaterial = new THREE.MeshStandardMaterial({ map: averagePlanetTexture });
+const averagePlanetMesh = new THREE.Mesh(averagePlanet, averagePlanetMaterial);
+scene.add(averagePlanetMesh)
+averagePlanetMesh.position.set(-((averagePlanetRadius + earthRadius) / 2) - spacing, 0, 0)
+
+var customMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        "c": { type: "f", value: 1.0 },
+        "p": { type: "f", value: 1.4 },
+        glowColor: { type: "c", value: new THREE.Color(0x08C8F3) },
+        viewVector: { type: "v3", value: camera.position }
+    },
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    side: THREE.FrontSide,
+    blending: THREE.AdditiveBlending,
+    transparent: true
+});
+var customMaterial2 = new THREE.ShaderMaterial({
+    uniforms: {
+        "c": { type: "f", value: 1.0 },
+        "p": { type: "f", value: 1.4 },
+        glowColor: { type: "c", value: new THREE.Color(0xfebbbb) },
+        viewVector: { type: "v3", value: camera.position }
+    },
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    side: THREE.FrontSide,
+    blending: THREE.AdditiveBlending,
+    transparent: true
+});
+let atmosphere = new THREE.Mesh(earth.clone(), customMaterial.clone());
+let atmosphere2 = new THREE.Mesh(averagePlanet.clone(), customMaterial2.clone());
+atmosphere.position.x = EarthMesh.position.x;
+atmosphere.position.y = EarthMesh.position.y;
+atmosphere.position.z = EarthMesh.position.z;
+atmosphere2.position.x = averagePlanetMesh.position.x;
+atmosphere2.position.y = averagePlanetMesh.position.y;
+atmosphere2.position.z = averagePlanetMesh.position.z;
+atmosphere2.scale.multiplyScalar(1.2);
+atmosphere.scale.multiplyScalar(1.2);
+scene.add(atmosphere, atmosphere2);
+
+function animate() {
+    requestAnimationFrame(animate);
+    atmosphere.material.uniforms.viewVector.value =
+        new THREE.Vector3().subVectors(camera.position, EarthMesh.position);
+    atmosphere2.material.uniforms.viewVector.value =
+        new THREE.Vector3().subVectors(camera.position, averagePlanetMesh.position);
+    EarthMesh.rotateY(0.003)
+
+    skyboxMesh.position.set(camera.position.x, camera.position.y, camera.position.z)
+    controls.update()
+
+    renderer.render(scene, camera);
+}
+animate()
---
+++ readDataBase.js
@@ -0,0 +1,39 @@
+import dbstring from "/dbstring.js"
+//grab string of all data from external file (abstraction) 
+let dbstringr = dbstring.replace(/[\r\n]/gm, "")
+    //use regex to find all return characters and remove them from the string
+let everyValueArray = dbstringr.split(",")
+    //split the string of all data into an array containing each data point
+let planets = []
+let dataBase = {}
+    //create the array that will contain each planets data
+for (let i = 0; i < (everyValueArray.length / 91); i++) {
+    planets.push([])
+}
+//use for loop to free up memory for the planets array's upcoming values
+for (let i = 0; i <= everyValueArray.length; i++) {
+    planets[Math.floor((i) / 92)].push(everyValueArray[i])
+        //loop through all data points and add them to an index in the planet
+        //the index at which the points go to is determined by there position in the all data points array devided by 
+        //the amount of data points a planet has
+
+    //see database_guide.txt for more info on data points relating to planets
+}
+let orbits = []
+for (let i = 0; i < planets.length; i++) {
+    orbits.push(planets[i][11])
+}
+
+let radii = []
+for (let i = 0; i < planets.length; i++) {
+    if (planets[i][19] != undefined) {
+        radii.push(planets[i][19])
+    }
+}
+
+
+dataBase.planets = planets;
+dataBase.orbits = orbits
+dataBase.radii = radii;
+
+export { dataBase }
